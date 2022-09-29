2e5afa5083c99e36fa7ab65b98579a7c52e0819b
Jason Simon <jason.simon422@gmail.com>
2022-09-29 13:14:16 +0200

    add localstorage and addToCart

--- front/js/product.js
+++ front/js/product.js
@@ -5,22 +5,28 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get("id");
 
+
+
 //console.log({ id })
 
 //Requete de l’API pour lui demander l’ensemble des informations pour un ID qui aura été récupéré depuis la page d'acceuil
 fetch(`http://localhost:3000/api/products/${id}`)
     .then ((response) => response.json())
     .then ((data) => createCard(data))
-
+ 
 
 //Fonction global qui sera toujours reprise dans le fetch
 //Génère toutes les constantes pour la création de la card page produit
 function createCard(kanap){
     //console.log(kanap)
-    const {imageUrl, altTxt, colors, name, price, _id, description} = kanap
+    const {imageUrl, altTxt, colors, name, price, description} = kanap
     addImage(imageUrl, altTxt)
+    imgUrl = imageUrl
+    altText = altTxt
+    articleName = name
     addName(name)
     addPrice(price)
+    itemPrice = price
     addDescription(description)
     addColors(colors)
 }
@@ -31,27 +37,27 @@ function addImage(imageUrl, altTxt){
     imgItem.src = imageUrl;
     imgItem.alt = altTxt;
     const parent = document.querySelector(".item__img")
-    if (parent != null) parent.appendChild(imgItem);
+    parent.appendChild(imgItem);
 }
 
 function addName(name){
     const h1 = document.querySelector("#title")
-    if (h1 != null) h1.textContent = name
+    h1.textContent = name
 }
 
 function addPrice(price){
     const span = document.querySelector("#price")
-    if (span != null) span.textContent = price
+    span.textContent = price
 }
 
 function addDescription(description){
     const p = document.querySelector("#description")
-    if (p != null) p.textContent = description
+    p.textContent = description
 }
 
 function addColors(colors){
     const select = document.querySelector("#colors")
-    if (select != null) select.textContent = colors
+    select.textContent = colors
     //console.log(colors)
     colors.forEach((color) => {
         const option = document.createElement("option")
@@ -63,7 +69,29 @@ function addColors(colors){
 }
 
 
-//Ajouter des produits dans le panier
-// ID , Quantité , Couleur , Prix total ?
-//Utiliser array / localstorage
-//Si produit identique (meme couleur) alors +1 qte
+//Ajout des éléments séléctionnés dans la page produit vers le localStorage
+//LocalStorage stock l'id la couleur l'image le prix et la quantité / Qte ne doit pas être égal a 0
+//Redirection après l'ajout au panier vers la page panier / Si erreur alors pas de redirection
+//A refactorer 
+
+const button = document.querySelector("#addToCart")
+button.addEventListener("click", (e) => {
+    const color = document.querySelector("#colors").value
+    const quantity = document.querySelector("#quantity").value
+    if(quantity == 0){
+        alert("Please select quantity")
+        return
+    }
+
+    const data = {
+        id: id,
+        name : articleName,
+        color: color,
+        quantity: quantity,
+        price: itemPrice,
+        altTxt: altText,
+        imageUrl : imgUrl
+    }
+    localStorage.setItem(id, JSON.stringify(data))
+    window.location.href = "cart.html"
+})
